25edf010b929f7b35d679b45b77332bf7116c5c6
sFlcn <sfalcn@gmail.com>
2022-01-04 19:58:11 +0300

    Рефакторинг js 1/2

--- source/js/script.js
+++ source/js/script.js
@@ -1,143 +1,187 @@
 'use strict';
 
+
 // Главное меню
-var navMain = document.querySelector(".main-nav");
-  var menuToggle = document.querySelector(".main-nav__menu-button");
 
-  navMain.classList.add("main-nav--closed");
-  navMain.classList.remove("main-nav--no-js");
+const mainNavButton = document.querySelector('.main-nav__menu-button');
+const mainNavElement = document.querySelector('.main-nav');
+const MAIN_NAV_CSS_CLASSES = ['main-nav--opened', 'main-nav--closed'];
 
-  menuToggle.addEventListener("click", function() {
-    if (navMain.classList.contains("main-nav--closed")) {
-      navMain.classList.remove("main-nav--closed");
-      navMain.classList.add("main-nav--opened");
-    } else {
-      navMain.classList.add("main-nav--closed");
-      navMain.classList.remove("main-nav--opened");
+const menuStarter = (
+  button,
+  menuElement,
+  cssClassesArray,
+) => {
+  button.addEventListener('click', () => {
+    for (const cssClass of cssClassesArray) {
+      menuElement.classList.toggle(cssClass);
     }
   });
+};
+
+mainNavElement.classList.add('main-nav--closed');
+mainNavElement.classList.remove('main-nav--no-js');
+menuStarter(mainNavButton, mainNavElement, MAIN_NAV_CSS_CLASSES);
+
+
+// Pop-up
+
+const POPUP_SHOW_CSS_CLASS = 'popup--show';
+
+const showPopup = (popupElement, popupCloseButton) => {
+
+  const closePopup = () => {
+    popupCloseButton.removeEventListener('click', closeButtonHandler);
+    document.removeEventListener('keydown', escKeydownHandler);
+    popupElement.classList.remove(POPUP_SHOW_CSS_CLASS);
+  };
+
+  const escKeydownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      closePopup();
+    }
+  };
+
+  const closeButtonHandler = (evt) => {
+    evt.preventDefault();
+    closePopup();
+  };
+
+  popupElement.classList.add(POPUP_SHOW_CSS_CLASS);
+  popupCloseButton.addEventListener('click', closeButtonHandler);
+  document.addEventListener('keydown', escKeydownHandler);
+};
 
 
 // Форма конкурса
-var contestForm = document.querySelector(".contest__form");
-var contestFormSuccesPopup = document.querySelector(".popup--succes");
-var contestFormFailurePopup = document.querySelector(".popup--failure");
 
-if (contestForm && contestFormSuccesPopup && contestFormFailurePopup) {
-  var contestFormRequiredElements = contestForm.querySelectorAll("[required]");
+const contestFormElement = document.querySelector('.contest__form');
+const contestFormSuccesPopupElement = document.querySelector('.popup--succes');
+const contestFormSuccesPopupCloseButton = document.querySelector('.popup--succes .popup__submit');
+const contestFormFailurePopupElement = document.querySelector('.popup--failure');
+const contestFormFailurePopupCloseButton = document.querySelector('.popup--failure .popup__submit');
+const INPUT_WARNING_CSS_CLASS = 'form__warning';
 
-  for (i = 0; i < contestFormRequiredElements.length; i++) {
-    contestFormRequiredElements[i].removeAttribute("required");
+
+const formStarter = (
+  formElement,
+  inputWarningCssClass,
+  succesPopupElement,
+  succesPopupCloseButton,
+  failurePopupElement,
+  failurePopupCloseButton,
+) => {
+  const formRequiredElements = formElement.querySelectorAll('[required]');
+
+  for (const element of formRequiredElements) {
+    element.removeAttribute('required');
   }
 
-  contestForm.addEventListener("submit", function (evt) {
-    var missingRequired = false;
-    for (i = contestFormRequiredElements.length - 1; i >= 0; i--) {
-      console.log(i);
-      contestFormRequiredElements[i].classList.remove("form__warning");
-      if (!contestFormRequiredElements[i].value) {
+  const formSubmitHandler = (evt) => {
+    let missingRequired = false;
+
+    for (let i = formRequiredElements.length - 1; i >= 0; i--) {
+      formRequiredElements[i].classList.remove(inputWarningCssClass);
+
+      if (!formRequiredElements[i].value) {
         missingRequired = true;
-        contestFormRequiredElements[i].classList.add("form__warning");
-        contestFormRequiredElements[i].scrollIntoView();
+        formRequiredElements[i].classList.add(inputWarningCssClass);
+        formRequiredElements[i].scrollIntoView();
       }
     }
+
     if (missingRequired) {
       evt.preventDefault();
-      contestFormFailurePopup.classList.add("popup--show");
+      showPopup(succesPopupElement, succesPopupCloseButton);
     } else {
-      contestFormSuccesPopup.classList.add("popup--show");
-      }
-  });
-
-  var succesPopupClose = contestFormSuccesPopup.querySelector(".popup__submit");
-  succesPopupClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    contestFormSuccesPopup.classList.remove("popup--show");
-  });
-
-  var failurePopupClose = contestFormFailurePopup.querySelector(".popup__submit");
-  failurePopupClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    contestFormFailurePopup.classList.remove("popup--show");
-  });
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (contestFormSuccesPopup.classList.contains("popup--show") || contestFormFailurePopup.classList.contains("popup--show")) {
-        contestFormSuccesPopup.classList.remove("popup--show");
-        contestFormFailurePopup.classList.remove("popup--show");
-      }
+      showPopup(failurePopupElement, failurePopupCloseButton);
+      formElement.reset();
     }
-  });
+  };
+
+  formElement.addEventListener('submit', formSubmitHandler);
+};
+
+if (contestFormElement && contestFormSuccesPopupElement && contestFormFailurePopupElement) {
+  formStarter(
+    contestFormElement,
+    INPUT_WARNING_CSS_CLASS,
+    contestFormSuccesPopupElement,
+    contestFormSuccesPopupCloseButton,
+    contestFormFailurePopupElement,
+    contestFormFailurePopupCloseButton,
+  );
 }
 
 
 // фотоальбом
-var album = document.querySelector(".album");
+
+const album = document.querySelector('.album');
 
 if (album) {
-  var picturePopup = document.querySelector(".popup--photo");
-  var popupFullPicture = picturePopup.querySelector(".popup__full-picture");
-  var popupDownloadLink = picturePopup.querySelector(".popup__download--file");
-  var popupCancel = picturePopup.querySelector(".popup__download--cancel");
-  var thumbnails = album.querySelectorAll(".album__photo-link");
-  var hearts = album.querySelectorAll(".likes__button");
-  var likesNumber = album.querySelectorAll(".likes__count");
+  const picturePopupElement = document.querySelector('.popup--photo');
+  const picturePopupImage = picturePopupElement.querySelector('.popup__full-picture');
+  const picturePopupDownloadLink = picturePopupElement.querySelector('.popup__download--file');
+  const picturePopupCloseButton = picturePopupElement.querySelector('.popup__download--cancel');
+  const albumThumbnails = album.querySelectorAll('.album__photo-link');
+  const albumHearts = album.querySelectorAll('.likes__button');
+  const albumLikesCounter = album.querySelectorAll('.likes__count');
 
-  var addThumbnailClickHandler = function (thumbnail, photo) {
-    thumbnail.addEventListener("click", function (evt) {
+  const thumbnailClickHandler = (thumbnail, photo) => {
+    thumbnail.addEventListener('click', function (evt) {
       evt.preventDefault();
-      popupFullPicture.src = photo;
-      popupDownloadLink.href = photo;
-      picturePopup.classList.add("popup--show");
+      picturePopupImage.src = photo;
+      picturePopupDownloadLink.href = photo;
+      picturePopupElement.classList.add('popup--show');
     });
   };
 
-  for (var i = 0; i < thumbnails.length; i++) {
-    var photoUrl = thumbnails[i].href;
-    addThumbnailClickHandler(thumbnails[i], photoUrl);
-    popupCancel.addEventListener("click", function (evt) {
+  for (var i = 0; i < albumThumbnails.length; i++) {
+    var photoUrl = albumThumbnails[i].href;
+    thumbnailClickHandler(albumThumbnails[i], photoUrl);
+    picturePopupCloseButton.addEventListener('click', function (evt) {
       evt.preventDefault();
-      picturePopup.classList.remove("popup--show");
+      picturePopupElement.classList.remove('popup--show');
     });
   }
 
   var addLikesClickHandler = function (likeButton, counter) {
-    likeButton.addEventListener("click", function (evt) {
+    likeButton.addEventListener('click', function (evt) {
       evt.preventDefault();
-      if (likeButton.classList.contains("likes__button--liked")) {
+      if (likeButton.classList.contains('likes__button--liked')) {
         counter.textContent--;
       } else {
         counter.textContent++;
       }
-      likeButton.classList.toggle("likes__button--liked");
+      likeButton.classList.toggle('likes__button--liked');
       document.activeElement.blur();
     });
   };
 
-  for (var i = 0; i < hearts.length; i++) {
-    addLikesClickHandler(hearts[i], likesNumber[i]);
+  for (var i = 0; i < albumHearts.length; i++) {
+    addLikesClickHandler(albumHearts[i], albumLikesCounter[i]);
   }
 }
 
 
 // фоторедактор
-var photoEditor = document.querySelector(".photo-editor");
+var photoEditor = document.querySelector('.photo-editor');
 
 if (photoEditor) {
-  var photoEditorTools = photoEditor.querySelectorAll(".photo-editor__tool");
-  var photoEditorRanges = photoEditor.querySelectorAll(".photo-editor__range");
+  var photoEditorTools = photoEditor.querySelectorAll('.photo-editor__tool');
+  var photoEditorRanges = photoEditor.querySelectorAll('.photo-editor__range');
 
   var addPhotoEditorToolsClickHandler = function (button, range) {
-    button.addEventListener("click", function (evt) {
+    button.addEventListener('click', function (evt) {
       evt.preventDefault();
       for (var i = 0; i < photoEditorTools.length; i++) {
-        photoEditorTools[i].classList.remove("photo-editor__tool--current");
-        photoEditorRanges[i].classList.remove("photo-editor__range--current");
+        photoEditorTools[i].classList.remove('photo-editor__tool--current');
+        photoEditorRanges[i].classList.remove('photo-editor__range--current');
       }
-      button.classList.add("photo-editor__tool--current");
-      range.classList.add("photo-editor__range--current");
+      button.classList.add('photo-editor__tool--current');
+      range.classList.add('photo-editor__range--current');
       document.activeElement.blur();
     });
   };
